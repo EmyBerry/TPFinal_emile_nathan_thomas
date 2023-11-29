d0967ebaf77d1285d496f181ef9756b6352d081a
Emily <emile.guerette1@hotmail.com>
2023-11-28 21:25:25 -0500

    obj detection script fini

--- Assets/ScriptableObjects/PlayerInfos.cs
+++ Assets/ScriptableObjects/PlayerInfos.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlayerInfos", menuName = "SO/PlayerInfos")]
 public class PlayerInfos : ScriptableObject {
 
-    public float ammountObjectScene1;
-    public float ammountObjectScene2;
-    public float ammountObjectScene3;
+    public float points;
     public float gameTime;
 }
